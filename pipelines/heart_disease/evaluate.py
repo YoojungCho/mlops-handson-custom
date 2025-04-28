808b12a62cecb2446e3a5f279cbedd28097d70e6
yoojung <youjungcho@mz.co.kr>
2025-04-28 06:26:12 +0000

    modify mse to accuracy

--- pipelines/heart_disease/evaluate.py
+++ pipelines/heart_disease/evaluate.py
@@ -51,7 +51,7 @@ if __name__ == "__main__":
     output_dir = "/opt/ml/processing/evaluation"
     pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
 
-    logger.info("Writing out evaluation report with mse: %f", mse)
+    logger.info("Writing out evaluation report with accuracy: %f", accuracy)
     evaluation_path = f"{output_dir}/evaluation.json"
     with open(evaluation_path, "w") as f:
         f.write(json.dumps(report_dict))
